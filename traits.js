1035e5642771a8067d6aa7a9863e1d3d039dbef4
Lucas Isabel <lucky.o.isa@gmail.com>
2016-04-21 21:44:45 -0300

    First commit with initial function

---
+++ traits.js
@@ -0,0 +1,12 @@
+function firstLetterNameUpperCase(variable){
+    var arrayNames = variable.trim().split(" ");
+
+    var i = 0;
+
+    while (i < arrayNames.length){
+        arrayNames[i] =  arrayNames[i].slice(0,1).toUpperCase() + arrayNames[i].slice(1).toLowerCase();
+        i++;
+    }
+
+    return arrayNames.join(" ");
+}
