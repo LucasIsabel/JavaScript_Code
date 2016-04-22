5ec431f821ceab8fe5ad336d203f6673e2858a07
Lucas Isabel <lucky.o.isa@gmail.com>
2016-04-21 21:55:15 -0300

    Reformatted Code

--- traits.js
+++ traits.js
@@ -1,10 +1,10 @@
-function firstLetterNameUpperCase(variable){
+function firstLetterNameUpperCase(variable) {
     var arrayNames = variable.trim().split(" ");
 
     var i = 0;
 
-    while (i < arrayNames.length){
-        arrayNames[i] =  arrayNames[i].slice(0,1).toUpperCase() + arrayNames[i].slice(1).toLowerCase();
+    while (i < arrayNames.length) {
+        arrayNames[i] = arrayNames[i].slice(0, 1).toUpperCase() + arrayNames[i].slice(1).toLowerCase();
         i++;
     }
 
